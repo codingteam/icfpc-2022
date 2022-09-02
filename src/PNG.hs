4fcdc78fd54c94bc6d9b07b012a0ea0f09f0dc18
Ilya Portnov <portnov@bk.ru>
2022-09-02 23:52:36 +0500

    Fix average.

--- src/PNG.hs
+++ src/PNG.hs
@@ -23,21 +23,19 @@ readPngImage path = do
 data PixelRGBAF = PixelRGBAF Float Float Float Float
   deriving (Eq, Show)
 
-pixelPlus :: PixelRGBAF -> PixelRGBA16 -> PixelRGBAF
-pixelPlus (PixelRGBAF r1 g1 b1 a1) (PixelRGBA16 r2 g2 b2 a2) =
+pixelPlus :: PixelRGBAF -> PixelRGBA8 -> PixelRGBAF
+pixelPlus (PixelRGBAF r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) =
   PixelRGBAF (r1 + fromIntegral r2) (g1 + fromIntegral g2) (b1 + fromIntegral b2) (a1 + fromIntegral a2)
 
 calcAvgColor :: Image PixelRGBA8 -> Shape -> Color
 calcAvgColor img shape =
-  let img16 = promoteImage img :: Image PixelRGBA16
-      summate acc x y pixel
+  let summate acc x y pixel
         | x >= rX shape && x < (rX shape + rWidth shape) && y >= rY shape && y < (rY shape + rHeight shape) = pixelPlus acc pixel
         | otherwise = acc
-      PixelRGBAF sumR sumG sumB sumA = pixelFold summate (PixelRGBAF 0 0 0 0) img16
+      PixelRGBAF sumR sumG sumB sumA = pixelFold summate (PixelRGBAF 0 0 0 0) img
       -- size = 1
       size = fromIntegral (imageWidth img * imageHeight img) :: Float
       (avgR, avgG, avgB, avgA) = (sumR / size, sumG / size, sumB / size, sumA / size)
-      demote p = round (p / 256)
-      colorAvg = PixelRGBA8 (demote avgR) (demote avgG) (demote avgB) (demote avgA)
+      colorAvg = PixelRGBA8 (round avgR) (round avgG) (round avgB) 255
   in colorAvg
 
