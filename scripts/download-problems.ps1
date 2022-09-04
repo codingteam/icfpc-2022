2e2f50405fdf7cc7dcff6aa2330b6724440477aa
Friedrich von Never <friedrich@fornever.me>
2022-09-04 19:06:18 +0200

    Add new problems, fresh specification

--- scripts/download-problems.ps1
+++ scripts/download-problems.ps1
@@ -8,4 +8,12 @@ foreach ($i in 1..$ProblemCount) {
     $url = "$ProblemApiBase/$i.png"
     Write-Host $url
     Invoke-RestMethod $url -OutFile "$OutputDirectory/$i.png"
+
+    Write-Host $url
+    $url = "$ProblemApiBase/$i.initial.json"
+    Invoke-RestMethod $url -OutFile "$OutputDirectory/$i.initial.json"
+
+    Write-Host $url
+    $url = "$ProblemApiBase/$i.source.png"
+    Invoke-RestMethod $url -OutFile "$OutputDirectory/$i.source.png"
 }
