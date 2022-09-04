df001949e1dec80865e754c482bee56b9c8d3ad3
💮 <t.me/pink_snow>
2022-09-04 02:34:51 +0000

    Speedup imageSimilarity

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -8,6 +8,7 @@ import Codec.Picture.Types
 import Types
 import Util
 import PNG (pixelToFloat, pixelDiff, PixelRGBAF (..), subImage)
+import Data.List (foldl')
 
 type Cost = Int
 
@@ -41,18 +42,31 @@ imageToVectorF img =
       [redF, greenF, blueF, alphaF] = map (V.map fromIntegral) [red, green, blue, alpha]
   in  V.zipWith4 (\r g b a -> V.fromList [r,g,b,a]) redF greenF blueF alphaF
 
-sqr :: Double -> Double
+pixelZipFold :: (Pixel pixel)
+          => (acc -> Int -> Int -> pixel -> pixel -> acc)
+          -> acc -> Image pixel -> Image pixel -> acc
+pixelZipFold
+      f initialAccumulator
+      img1@(Image { imageWidth = w, imageHeight = h })
+      img2@(Image { imageWidth = w2, imageHeight = h2 }) =
+    -- TODO: assert w == w2 && h == h2
+    foldl' columnFold initialAccumulator [0 .. h - 1]
+      where
+        pixelZipFolder y acc x = f acc x y (pixelAt img1 x y) (pixelAt img2 x y)
+        columnFold lineAcc y = foldl' (pixelZipFolder y) lineAcc [0 .. w - 1]
+
+sqr :: (Num a) => a -> a
 sqr x = x*x
 
+pixelSimilarity :: PixelRGBA8 -> PixelRGBA8 -> Float
+pixelSimilarity p1 p2 =
+  let PixelRGBAF r1 g1 b1 a1 = pixelToFloat p1
+      PixelRGBAF r2 g2 b2 a2 = pixelToFloat p2
+  in sqrt $ sqr (r1 - r2) + sqr (g1 - g2) + sqr (b1 - b2) + sqr (a1 - a2)
+
 imageSimilarity :: Image PixelRGBA8 -> Image PixelRGBA8 -> Cost
-imageSimilarity img1 img2 =
-  let vector1 = imageToVectorF img1
-      vector2 = imageToVectorF img2
-      diff = V.zipWith (V.zipWith (-)) vector1 vector2
-      pixelDistances = V.map (sqrt . V.sum . V.map sqr) diff
-      sumDistance = V.sum pixelDistances
-      alpha = 0.005
-  in  jsRound $ alpha * sumDistance
+imageSimilarity img1 img2 = jsRound $ realToFrac $ 0.005 * pixelZipFold f 0 img1 img2
+  where f acc _ _ p1 p2 = acc + pixelSimilarity p1 p2
 
 pixelNorm :: PixelRGBAF -> Float
 pixelNorm (PixelRGBAF r g b a) = sqrt $ r*r + g*g + b*b + a*a
